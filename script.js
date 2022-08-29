14febb8e8cad0d15e2d6bf6e2b26295f90d569d2
David Lindner Jr <davidlindnerjr@Davids-MacBook-Air.local>
2022-08-29 12:57:13 -0700

    Password Auth to parentPortal

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// BG IMAGE CAROUSEL
-
+// CAROUSEL
 let images = ['./assets/banner_image_1.png', './assets/banner_image_2.png'];
 let nextimage = 0;
 
@@ -19,20 +18,37 @@ doSlideshow();
 // MODAL
 let modal = document.querySelector(".modal")
 let closeBtn = document.querySelector(".close-btn")
+let parentPortalBtn = document.querySelector("#parentPortalBtn")
 
-    $(window).on('load',() => {
-        const loadModal = () => {
-            modal.style.display = "block"
-        }
-        window.setTimeout(loadModal, 3000);
-    });
+$(window).on('load',() => {
+    const loadModal = () => {
+        modal.style.display = "block"
+    }
+    window.setTimeout(loadModal, 3000);
+});
+
+$(window).on('click', (e) => {
+    if(e.target == modal){
+        modal.style.display = "none"
+    }
+});
 
 closeBtn.onclick = () => {
   modal.style.display = "none"
 }
 
-$(window).on('click', (e) => {
-  if(e.target == modal){
-    modal.style.display = "none"
-  }
+parentPortalBtn.onclick =  () => {
+    modal.style.display = "block"
+}
+
+// PASSWORD AUTH
+$("#passwordBtn").on('click', (e) => {
+    e.preventDefault();
+    let str = $("#passwordInput").val();
+
+    if(str === 'password123'){
+        window.location.href = 'parentPortal.html';
+    } else {
+        alert('Incorrect Password');
+    }
 });
